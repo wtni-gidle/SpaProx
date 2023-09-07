9634253ded48b3bb71152a12d7bbb5527e025614
wtni-gidle <81308718+wtni-gidle@users.noreply.github.com>
2023-09-07 19:35:26 +0800

    update

--- model/models.py
+++ model/models.py
@@ -51,71 +51,67 @@ class MLP(nn.Module):
         return x
     
 
+# class Attention(nn.Module):
+#     # 自注意力+一层全连接（含两层dropout），不含layernorm和add
+#     def __init__(
+#         self,
+#         num_heads,
+#         all_head_size,
+#         input_size,
+#         attn_dropout_rate = 0.0,
+#         proj_dropout_rate = 0.0
+#     ) -> None:
+#         super().__init__()
+#         self.num_heads = num_heads
+#         self.all_head_size = all_head_size
+#         self.head_size = int(all_head_size / num_heads)
+#         if input_size is None:
+#             input_size = all_head_size
+#         self.query = nn.Linear(input_size, self.all_head_size)
+#         self.key = nn.Linear(input_size, self.all_head_size)
+#         self.value = nn.Linear(input_size, self.all_head_size)
 
+#         self.attn_drop = nn.Dropout(p = attn_dropout_rate)
+#         self.proj = nn.Linear(self.all_head_size, self.all_head_size)
+#         self.proj_drop = nn.Dropout(p = proj_dropout_rate)
 
-class Customformer(nn.Module):
-    pass
+#     def forward(self, input):
+#         # 获取qkv
+#         query_layer = self.query(input)
+#         key_layer = self.key(input)
+#         value_layer = self.value(input)
+#         query_layer = self.transpose_for_scores(query_layer)
+#         key_layer = self.transpose_for_scores(key_layer)
+#         value_layer = self.transpose_for_scores(value_layer)
+#         # 计算注意力分数+dropout
+#         attn_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2)) / sqrt(self.head_size)
+#         attn_probs = nn.Softmax(dim = -1)(attn_scores)
+#         attn_probs = self.attn_drop(attn_probs)
+#         # 得到注意力输出
+#         context_layer = torch.matmul(attn_probs, value_layer)
+#         context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+#         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+#         context_layer = context_layer.view(*new_context_layer_shape)
+#         # 全连接+dropout
+#         output = self.proj(context_layer)
+#         output = self.proj_drop(context_layer)
+
+#         return output
+    
+#     def transpose_for_scores(self, x: torch.Tensor):
+#         new_x_shape = x.size()[:-1] + (self.num_heads, self.head_size)
+#         x = x.view(*new_x_shape)
+
+#         return x.permute(0, 2, 1, 3)
+
 
 
 class Attention(nn.Module):
-    # 自注意力+一层全连接（含两层dropout），不含layernorm和add
-    def __init__(
-        self,
-        num_heads,
-        all_head_size,
-        input_size,
-        attn_dropout_rate = 0.0,
-        proj_dropout_rate = 0.0
-    ) -> None:
-        super().__init__()
-        self.num_heads = num_heads
-        self.all_head_size = all_head_size
-        self.head_size = int(all_head_size / num_heads)
-        if input_size is None:
-            input_size = all_head_size
-        self.query = nn.Linear(input_size, self.all_head_size)
-        self.key = nn.Linear(input_size, self.all_head_size)
-        self.value = nn.Linear(input_size, self.all_head_size)
-
-        self.attn_drop = nn.Dropout(p = attn_dropout_rate)
-        self.proj = nn.Linear(self.all_head_size, self.all_head_size)
-        self.proj_drop = nn.Dropout(p = proj_dropout_rate)
-
-    def forward(self, input):
-        # 获取qkv
-        query_layer = self.query(input)
-        key_layer = self.key(input)
-        value_layer = self.value(input)
-        query_layer = self.transpose_for_scores(query_layer)
-        key_layer = self.transpose_for_scores(key_layer)
-        value_layer = self.transpose_for_scores(value_layer)
-        # 计算注意力分数+dropout
-        attn_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2)) / sqrt(self.head_size)
-        attn_probs = nn.Softmax(dim = -1)(attn_scores)
-        attn_probs = self.attn_drop(attn_probs)
-        # 得到注意力输出
-        context_layer = torch.matmul(attn_probs, value_layer)
-        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
-        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
-        context_layer = context_layer.view(*new_context_layer_shape)
-        # 全连接+dropout
-        output = self.proj(context_layer)
-        output = self.proj_drop(context_layer)
-
-        return output
-    
-    def transpose_for_scores(self, x: torch.Tensor):
-        new_x_shape = x.size()[:-1] + (self.num_heads, self.head_size)
-        x = x.view(*new_x_shape)
-
-        return x.permute(0, 2, 1, 3)
-
-
-
-class Encoder(nn.Module):
+    # 自注意力+一层全连接（含两个dropout）
+    #! x:(n, k) -> (n, k)
     def __init__(
         self, 
-        embed_dim, 
+        d_model, 
         num_heads, 
         attn_drop_rate = 0.0, 
         proj_drop_rate = 0.0, 
@@ -125,7 +121,7 @@ class Encoder(nn.Module):
         super().__init__()
 
         self.self_attn = nn.MultiheadAttention(
-            embed_dim, 
+            d_model, 
             num_heads, 
             attn_drop_rate, 
             batch_first = batch_first, 
@@ -138,3 +134,61 @@ class Encoder(nn.Module):
         attn_output = self.dropout(attn_output)
 
         return attn_output
+    
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_hidden, drop_rate) -> None:
+        super().__init__()
+        self.fc1 = nn.Linear(d_model, d_hidden)
+        self.gelu = nn.GELU()
+        self.dropout1 = nn.Dropout(drop_rate)
+        self.fc2 = nn.Linear(d_hidden, d_model)
+        self.dropout2 = nn.Dropout(drop_rate)
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.gelu(x)
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        x = self.dropout2(x)
+
+        return x
+    
+class EncoderLayer(nn.Module):
+    # 多头注意力+add&norm+fnn+add&norm
+    #! 想要x:(n, k) -> (n, d_model)
+    def __init__(self, d_model, num_heads, attn_drop_rate, hidden_drop_rate, d_hidden) -> None:
+        super().__init__()
+        self.attn = Attention(d_model, num_heads, attn_drop_rate, hidden_drop_rate)
+        self.layer_norm1 = nn.LayerNorm(d_model)
+        self.ffn = FeedForward(d_model, d_hidden, hidden_drop_rate)
+        self.layer_norm2 = nn.LayerNorm(d_model)
+
+    def forward(self, x):
+        atten_output = self.attn(x)
+        x = x + atten_output
+        x = self.layer_norm1(x)
+
+        ffn_output = self.ffn(x)
+        x = x + ffn_output
+        x = self.layer_norm2(x)
+
+        return x
+
+class Customformer(nn.Module):
+    def __init__(self, d_model, num_heads, attn_drop_rate, hidden_drop_rate, d_hidden, num_features) -> None:
+        super().__init__()
+        self.proj_ascend = nn.Linear(1, d_model)
+        self.encoder = EncoderLayer(d_model, num_heads, attn_drop_rate,  hidden_drop_rate, d_hidden)
+        self.proj_reduce = nn.Linear(d_model, 1)
+        self.flatten = nn.Flatten()
+        self.classifier = MLP([num_features, 64])
+        self.loss_fn = self.classifier.loss_fn
+
+    def forward(self, x):
+        x = self.proj_ascend(x)
+        x = self.encoder(x)
+        x = self.proj_reduce(x)
+        x = self.flatten(x)
+        x = self.classifier(x)
+
+    
